3e73a5c3f92947c72b4d3abb2188f073c88f9829
skyhightech55 <Mr.skys0711@softbank.ne.jp>
2022-06-06 05:38:05 +0900

    ユーザー管理機能の実装

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '7d7bed23c3d62d8e87ec0b927f200a59be4c72ce04bac93d965d87eaaf793e3cc8c27f2e477aeaa44cfbcb9056b8b3e84c3b42eee428f5b944be890854652b9b'
